650dd1c433e3d453a1a9f5294362de8c554982d8
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-13 22:24:49 +0100

    Fixed iPhone sound issue

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -19,10 +19,20 @@ class ViewController: UIViewController {
     
     func playSound(key: String) {
         guard let url = Bundle.main.url(forResource: key, withExtension: "wav") else { return }
-        player = try! AVAudioPlayer(contentsOf: url)
-        guard let player = player else { return }
-        player.prepareToPlay()
-        player.play()
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
     
     @IBAction func keyPressed(_ sender: UIButton) {
